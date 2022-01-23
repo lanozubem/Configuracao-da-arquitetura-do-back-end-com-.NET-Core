e676c6bc7f08920671db3fec5d993c8400c5b493
lanozubem <zubemluano@gmail.com>
2022-01-23 10:22:45 -0300

    Apresentação do cenário e documentação da API com swagger

---
+++ Curso.API/Filters/ValidacaoModelStateCustomizado.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace curso.api.Models
+{
+    public class ValidacaoModelStateCustomizado : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var validaCampoViewModel = new ValidaCampoViewModeOutput();
+                context.Result = new BadRequestObjectResult(validaCampoViewModel);
+            }
+        }
+    }
+}
---
+++ Curso.API/Models/Usuarios/LoginViewModelInput.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Models
+{
+    public class LoginViewModelInput
+    {
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
